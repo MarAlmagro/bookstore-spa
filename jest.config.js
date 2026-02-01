be9c2b8c9153ed60c50b6bf07ca3b2280d46fc07
Mar Almagro Moreno <maralmagromoreno@gmail.com>
2026-02-01 18:11:31 +0100

    chore(test): replace Karma with Jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.routes.ts',
+    '!src/main.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@environments/(.*)$': '<rootDir>/src/environments/$1'
+  }
+};
