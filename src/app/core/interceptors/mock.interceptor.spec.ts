4579f10b643833c85b4ff781a1b98fbe1fefd70c
Mar Almagro Moreno <maralmagromoreno@gmail.com>
2026-02-01 17:54:15 +0100

    feat(core): add MockInterceptor for offline dev

---
+++ src/app/core/interceptors/mock.interceptor.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpRequest, HttpHandler } from '@angular/common/http';
+import { of } from 'rxjs';
+import { MockInterceptor } from './mock.interceptor';
+
+describe('MockInterceptor', () => {
+  let interceptor: MockInterceptor;
+  let httpHandlerMock: Partial<HttpHandler>;
+
+  beforeEach(() => {
+    httpHandlerMock = {
+      handle: jest.fn().mockReturnValue(of({}))
+    };
+
+    TestBed.configureTestingModule({
+      providers: [MockInterceptor]
+    });
+
+    interceptor = TestBed.inject(MockInterceptor);
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should pass through when mocks disabled', (done) => {
+    const request = new HttpRequest('GET', '/api/v1/books');
+
+    interceptor.intercept(request, httpHandlerMock as HttpHandler).subscribe(() => {
+      expect(httpHandlerMock.handle).toHaveBeenCalledWith(request);
+      done();
+    });
+  });
+});
